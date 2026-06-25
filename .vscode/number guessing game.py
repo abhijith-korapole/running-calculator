f5662e412993b2456a58e9506d5cbc8b2ac30330
abhijith-korapole <abhijithkorapole0208@gmail.com>
2026-06-25 23:45:17 +0530

    nothing

---
+++ .vscode/number guessing game.py
@@ -0,0 +1,30 @@
+import random 
+print("welcome to the number guessing game!")
+print("im thinking of a number between 1-100")
+difficulty = input("choose the type of difficulty: easy/medium/hard: ").lower()
+if difficulty == "easy":
+    attempts = 10
+elif difficulty == "medium":
+    attempts = 7
+elif difficulty == "hard":
+    attempts = 5
+else:
+    print("invalid difficulty level. plase choose easy / medium / hard") 
+    exit()
+
+for i in range(attempts,0,-1):
+       print(f"you have {i} attempts to guess the number")
+       guess = int(input("make a guess: "))
+       computer_choice = random.randint(1,100)
+       if guess>computer_choice:
+        print("too high")
+       elif guess<computer_choice:
+        print("too low")
+       elif guess == computer_choice:
+          print(f"you win!")
+          break
+else:
+         print(f"you loose! the answer was {computer_choice}")
+         
+       
+       
